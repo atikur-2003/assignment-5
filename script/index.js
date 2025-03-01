b013808283378ca58b1bfe4bf538e0cb8a748443
Atikur Rahman <atikur947382@gmail.com>
2025-03-02 01:19:21 +0600

    added some logic

---
+++ script/index.js
@@ -0,0 +1,7 @@
+const discoverDiv = document.getElementById('discover-div');
+
+discoverDiv.addEventListener('click', function(){
+    // console.log("done");
+    window.location.href = "./blog.html";
+})
+
