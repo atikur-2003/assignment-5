7ff7085c2983275faf7a14b6ad993f554dcad104
Atikur Rahman <atikur947382@gmail.com>
2025-03-02 21:45:10 +0600

    added some logic

--- script/index.js
+++ script/index.js
@@ -1,7 +1,28 @@
 const discoverDiv = document.getElementById('discover-div');
 
 discoverDiv.addEventListener('click', function(){
-    // console.log("done");
     window.location.href = "./blog.html";
 })
 
+
+//generating random bg-color
+document.getElementById('change-bg').addEventListener('click', function(){
+    document.body.style.backgroundColor = getRandomColor();
+})
+
+function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 1; i <= 6; i++) {
+        color = color + letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// showing update date
+const date = new Date();
+document.getElementById('update-date').innerText = date.toDateString();
+
+document.querySelectorAll('.complete-btn').addEventListener('click', function(){
+    alert("done");
+})
